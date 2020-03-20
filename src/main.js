544c8a76dce000bd05d52d4c33b13c9af1638691
Canna Wen <cannawen@gmail.com>
2020-03-20 15:30:29 -0400

    Add test to determine if two sets of data have any overlap

--- src/main.js
+++ src/main.js
@@ -1,19 +1,22 @@
-function didUserVisit(data, targetPlaceId, startTime, endTime) {
-  return data
-    .timelineObjects
+function findOverlap(confirmedData, unconfirmedData) {
+  return [];
+}
+
+function didUserVisit(unconfirmedData, confirmedPlaceId, confirmedStartTime, confirmedEndTime) {
+  return unconfirmedData.timelineObjects
     .filter(timelineObject => timelineObject.placeVisit != undefined)
     .map(timelineObject => timelineObject.placeVisit)
-    .filter(placeVisit => placeVisit.location.placeId == targetPlaceId)
+    .filter(placeVisit => placeVisit.location.placeId == confirmedPlaceId)
     .filter(placeVisit => {
         const dataStartTime = placeVisit.duration.startTimestampMs;
         const dataEndTime = placeVisit.duration.endTimestampMs;
 
-        const userWasTherePriorToDataTime = endTime < dataStartTime;
-        const userWasThereAfterToDataTime = startTime > dataEndTime;
+        const unconfirmedUserWasTherePriorToConfirmedTime = confirmedEndTime < dataStartTime;
+        const unconfirmedUserWasThereAfterConfirmedTime = confirmedStartTime > dataEndTime;
 
-        return !(userWasThereAfterToDataTime || userWasTherePriorToDataTime);
+        return !(unconfirmedUserWasTherePriorToConfirmedTime || unconfirmedUserWasThereAfterConfirmedTime);
       }
     );
 }
 
-module.exports = {didUserVisit};
+module.exports = {didUserVisit, findOverlap};
--- test/main.test.js
+++ test/main.test.js
@@ -1,33 +1,46 @@
 let fs = require('fs');
 
-let {didUserVisit} = require('../src/main');
+let {didUserVisit, findOverlap} = require('../src/main');
 
-describe('Google Location History Takeout data file', () => {
-  describe('has user been to specific placeId', () => {
-    let testData;
+describe('Two data sets', () => {
+  let confirmedData;
+  let unconfirmedData;
 
-    beforeEach(() => {
-      testData = JSON.parse(fs.readFileSync('./test/data/data.json', 'utf8'))
-    })
+  beforeEach(() => {
+    confirmedData = JSON.parse(fs.readFileSync('./test/data/confirmedData.json', 'utf8'))
+    unconfirmedData = JSON.parse(fs.readFileSync('./test/data/unconfirmedData.json', 'utf8'))
+  })
 
-    test('user did not visit place at all', () => {
-      expect(didUserVisit(testData, "XYZTestPlaceId", 0, 1000000).length).toBe(0);
-    })
-
-    test('user visited before target time', () => {
-      expect(didUserVisit(testData, "ABCTestPlaceId", 0 ,500).length).toBe(0);
-    })
-
-    test('user visited after target time', () => {
-      expect(didUserVisit(testData, "ABCTestPlaceId", 2500, 3500).length).toBe(0);
-    })
-
-    test('user was there during target time', () => {      
-      expect(didUserVisit(testData, "ABCTestPlaceId", 900, 2100).length).toBe(1);
-      expect(didUserVisit(testData, "ABCTestPlaceId", 1100, 1900).length).toBe(1);
-      expect(didUserVisit(testData, "ABCTestPlaceId", 900, 1900).length).toBe(1);
-      expect(didUserVisit(testData, "ABCTestPlaceId", 1100, 2100).length).toBe(1);
-    })
-
+  test('find overlap', () => {
+    expect(findOverlap(confirmedData, unconfirmedData).length).toBe(2)
   })
 })
+
+describe('has user been to specific placeId', () => {
+  let testData;
+
+  beforeEach(() => {
+    testData = JSON.parse(fs.readFileSync('./test/data/unconfirmedData.json', 'utf8'))
+  })
+
+  test('user did not visit place at all', () => {
+    expect(didUserVisit(testData, "XYZTestPlaceId", 0, 1000000).length).toBe(0);
+  })
+
+  test('user visited before target time', () => {
+    expect(didUserVisit(testData, "ABCTestPlaceId", 0 ,500).length).toBe(0);
+  })
+
+  test('user visited after target time', () => {
+    expect(didUserVisit(testData, "ABCTestPlaceId", 2500, 3500).length).toBe(0);
+  })
+
+  test('user was there during target time', () => {      
+    expect(didUserVisit(testData, "ABCTestPlaceId", 900, 2100).length).toBe(1);
+    expect(didUserVisit(testData, "ABCTestPlaceId", 1100, 1900).length).toBe(1);
+    expect(didUserVisit(testData, "ABCTestPlaceId", 900, 1900).length).toBe(1);
+    expect(didUserVisit(testData, "ABCTestPlaceId", 1100, 2100).length).toBe(1);
+  })
+
+})
+
